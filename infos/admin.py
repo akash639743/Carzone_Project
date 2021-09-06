da3e1ca729640f11c7deea0dc333af868c4c6200
Akash <negiakash63@gmail.com>
2021-09-06 18:18:02 +0530

    My first commit

---
+++ infos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Info
+
+# Register your models here.
+class InfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'car_title', 'city', 'create_date')
+    list_display_links = ('id', 'first_name', 'last_name')
+    search_fields = ('first_name', 'last_name', 'email', 'car_title')
+    list_per_page = 25
+
+admin.site.register(Info, InfoAdmin)
---
+++ pages/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models.fields import CharField
+
+# Create your models here.
+
+class Team(models.Model):
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    designation=models.CharField(max_length=255)
+    photo=models.ImageField(upload_to="photo/%Y/%m/%d/")
+    facebook_link=models.URLField(max_length=255)
+    twitter_link=models.URLField(max_length=255)
+    google_link=models.URLField(max_length=255)
+    created_date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
+    
